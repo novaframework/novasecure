9ba06f95bae662afcd4131a481490b776fec17ff
Daniel Widgren <daniel.widgren@gmail.com>
2023-06-23 01:10:05 +0200

    fixed routing and plugins

--- src/controllers/novasecure_main_controller.erl
+++ src/controllers/novasecure_main_controller.erl
@@ -1,21 +1,21 @@
 -module(novasecure_main_controller).
 -export([
          get_provider/1,
-         get_provider_callback/1,
-         create_provider_callback/1,
+         get_callback/1,
+         post_callback/1,
          logout/1
         ]).
-
--include_lib("nova/include/nova.hrl").
 
 get_provider(#{bindings := #{<<"provider">> := Provider}}) ->
     provider_module(Provider),
     {status, 200}.
 
-get_provider_callback(_Req) ->
+get_callback(#{bindings := #{<<"provider">> := Provider}} = Req) ->
+    Module = provider_module(Provider),
+    Module:callback(Req),
     {status, 200}.
 
-create_provider_callback(_Req) ->
+post_callback(_Req) ->
     {status, 201}.
 
 logout(_Req) ->
--- src/novasecure_router.erl
+++ src/novasecure_router.erl
@@ -9,10 +9,13 @@
 routes(_Environment) ->
   [#{prefix => "",
   security => false,
+  plugins => [{pre_request, nova_request_plugin, #{decode_json_body => true,
+                                                   parse_qs => true
+                                                  }}],
   routes => [
              {"/:provider", { novasecure_main_controller, get_provider}, #{methods => [get]}},
-             {"/:provider/callback", { novasecure_main_controller, get_provider_callback}, #{methods => [get]}},
-             {"/:provider/callback", { novasecure_main_controller, create_provider_callback}, #{methods => [post]}},
+             {"/:provider/callback", { novasecure_main_controller, get_callback}, #{methods => [get]}},
+             {"/:provider/callback", { novasecure_main_controller, post_callback}, #{methods => [post]}},
              {"/logout", { novasecure_main_controller, logout}, #{methods => [delete]}}
            ]
       }].
