abfe6d45c78c52c94f608d279b49aba0f852f44e
Daniel Widgren <daniel.widgren@gmail.com>
2023-06-23 02:05:13 +0200

    Changed redirect for providers after successful login

--- src/controllers/novasecure_main_controller.erl
+++ src/controllers/novasecure_main_controller.erl
@@ -12,8 +12,9 @@ get_provider(#{bindings := #{<<"provider">> := Provider}}) ->
 
 get_callback(#{bindings := #{<<"provider">> := Provider}} = Req) ->
     Module = provider_module(Provider),
-    Module:callback(Req),
-    {status, 200}.
+    ok = Module:callback(Req),
+    #{redirect_url := Redirect} = application:get_env(novasecure, binary_to_atom(Provider), undefined),
+    {redirect, <<"/user">>}.
 
 post_callback(_Req) ->
     {status, 201}.
